58675f130eb9320fdd93d8bba18ecf97ff55ef79
monthdev <monthdevops@gmail.com>
2026-02-28 22:25:28 -0700

    Add unit tests

--- test/MacOS/TestSupportDataMacOS.cpp
+++ test/MacOS/TestSupportDataMacOS.cpp
@@ -4,7 +4,21 @@
 
 #include "Qt/App/SupportData.hpp"
 
+static const std::string EXPECTED_BASE_DIRECTORY_PATH{
+    QDir::homePath().toStdString() + "/Library/Application Support/Memly"
+};
+
 TEST(SupportDataMacOS, BaseDirectoryPath) {
     EXPECT_EQ(Qt::App::SupportData::BaseDirectoryPath(),
-              QDir::homePath().toStdString() + "/Library/Application Support");
+              EXPECTED_BASE_DIRECTORY_PATH);
+}
+
+TEST(SupportDataMacOS, AudioDirectoryPath) {
+    EXPECT_EQ(Qt::App::SupportData::AudioDirectoryPath(),
+              EXPECTED_BASE_DIRECTORY_PATH + "/Audio");
+}
+
+TEST(SupportDataMacOS, DatabaseFilePath) {
+    EXPECT_EQ(Qt::App::SupportData::DatabaseFilePath(),
+              EXPECTED_BASE_DIRECTORY_PATH + "/memly.duckdb");
 }
