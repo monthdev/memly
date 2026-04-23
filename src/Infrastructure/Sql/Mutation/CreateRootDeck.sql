f40b342690270f496470e3c741d5b6df3c22fbe1
monthdev <monthdevops@gmail.com>
2026-04-23 14:20:46 -0700

    Apply sql format

--- src/Infrastructure/Sql/Mutation/CreateRootDeck.sql
+++ src/Infrastructure/Sql/Mutation/CreateRootDeck.sql
@@ -10,11 +10,7 @@ WITH
       1
   )
 INSERT INTO
-  decks (
-    name,
-    target_language_code,
-    deck_settings_id
-  )
+  decks (name, target_language_code, deck_settings_id)
 SELECT
   ?,
   ?,
